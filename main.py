b8ffe447d6bd159eb95ebc8576e3c1a75819abe3
Nicolas Far <nicofff@gmail.com>
2013-09-22 15:37:36 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+class TseClient:
+    def connect(self, endpoint):
+        #Ready connection for data transfer
+        # validate()
+        #negotiateProtocol()
+        return
+    
+    def validate (endPoint, identity):
+        # Comunicate with trusted Endpoint/s and valdiate identity
+        return
+        
+    def negociateProtocol(self):
+        #Setup crypto protocol
+        return
+    
+    def send(self, buffer):
+        return
+    
+    def receive (self,  buffer):
+        return
+    
+
+class TseServer:
+    def listen(self):
+        return
+    
+class Endpoint:
+    def init(self, host, port):
+        return
